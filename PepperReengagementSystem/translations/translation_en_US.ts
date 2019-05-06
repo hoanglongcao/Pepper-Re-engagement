98d5bcba82874ede43bc20a3a728222de0fc27ca
Hoang-Long Cao <chlong.mechatronics@gmail.com>
2019-05-06 16:09:32 +0200

    Add Choregraphe project

---
+++ PepperReengagementSystem/translations/translation_en_US.ts
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>FinalProyect2.2/Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>FinalProyect2.3/FinalProyect2.2/Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>FinalProyect2.3/Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>FinalProyect2.Final/FinalProyect2.2/Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>FinalProyect2.Final/FinalProyect2.3/FinalProyect2.2/Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>FinalProyect2.Final/FinalProyect2.3/Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>FinalProyect2.Final/Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>Valence25/behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Values</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Values</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/AffectiveState/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Error</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Error</translation>
+        </message>
+        <message>
+            <source>gaze</source>
+            <comment>Text</comment>
+            <translation type="obsolete">gaze</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/UserExpression/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Expression</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Expression</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/UserExpression/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Error</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Error</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/UserMood/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Mood</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Mood</translation>
+        </message>
+    </context>
+</TS>
